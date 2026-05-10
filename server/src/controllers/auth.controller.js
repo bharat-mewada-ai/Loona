d55c75b418acfc6b988108854d2d678dffc6c9ab
bharat-mewada-ai <bharatmewada652@gmail.com>
2026-05-10 22:33:16 +0530

    fix(server): add missing bcrypt import for login controller

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import axios from "axios";
 import { OAuth2Client } from "google-auth-library";
 import { checkContent } from "../utils/moderation.js";
